ddf2b85c2f4f443b94a6ba0cedff7978bce82116
Taras <tvsy6520@gmail.com>
2021-01-11 01:20:04 +0200

    Store: renamed state variables for config

--- store/modules/atm/config.js
+++ store/modules/atm/config.js
@@ -1,12 +1,12 @@
 const state = () => ({
-  limitOfGiveOut: 2000,
-  defaultVariantsOfGiveOut: [100, 200, 500, 1000]
+  withdrawalLimit: 2000,
+  defaultWithdrawalVariants: [100, 200, 500, 1000]
 })
 
 const getters = {
-  LIMIT_OF_GIVE_OUT: state => state.limitOfGiveOut,
-  DEFAULT_VARIANTS_OF_GIVE_OUT: (state) => {
-    return state.defaultVariantsOfGiveOut.sort((current, next) => current > next ? 1 : -1)
+  WITHDRAWAL_LIMIT: state => state.withdrawalLimit,
+  DEFAULT_WITHDRAWAL_VARIANTS: (state) => {
+    return state.defaultWithdrawalVariants.sort((current, next) => current > next ? 1 : -1)
   }
 }
 
