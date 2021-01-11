3653ddbcb527d134f57bbb21e531eed2c401045b
Taras <tvsy6520@gmail.com>
2021-01-11 03:49:17 +0200

    Plugins: Installed vuelidate plugin for validate forms

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    {src: '~/plugins/bootstrap-icons'}
+    {src: '~/plugins/bootstrap-icons'},
+    {src: '~/plugins/vuelidate'}
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
